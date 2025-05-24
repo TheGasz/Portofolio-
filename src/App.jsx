b247ffb6b524ed6357169de08ba66b19c9d2e85b
TheGasz <bagasadjiepamungkas2006@mail.ugm.ac.id>
2025-05-24 20:44:35 +0700

    react init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import "./index.css";
+import { header } from "./assets/header";
+
+function App() {
+  return (
+   <header tittle="Hello World" />
+    
+     
+    
+  );
+}
+
+export default App;
---
+++ src/assets/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "..assets/index.css";
+
+
+export const header = ({tittle}) => {
+  return (
+    <div className='bg-red-500'>{tittle}</div>
+
+    
+  )
+}
